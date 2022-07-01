eb7679aeee174adb7228dd7d59e9f1737f4c01c4
zeroincombenze <info@shs-av.com>
2022-07-01 15:25:20 +0200

    [SYNC] OCA synchronization

---
+++ sale_commission_plus/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Sales commissions plus",
+    "version": "10.0.2.6.0",
+    "author": "SHS-AV s.r.l.",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_commission",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+}
